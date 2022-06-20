e2fe768a5ef01a9d4816bea10badf6cc7916cb7e
Najd <fekinajd@gmail.com>
2022-06-20 15:16:10 +0100

    added deault exports

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const graphqlUploadExpress = require("./graphqlUploadExpress");
 const GraphQLUpload = require("./GraphQLUpload.js");
 const graphqlUploadKoa = require("./graphqlUploadKoa.js");
 const Upload = require("./Upload.js");
+const processRequest = require("./processRequest.js");
 const GRAPHQL_MULTIPART_REQUEST_SPEC_URL = require("./GRAPHQL_MULTIPART_REQUEST_SPEC_URL.js");
-module.exports = graphqlUploadExpress;
-module.exports = GraphQLUpload;
-module.exports = graphqlUploadKoa;
-module.exports = GraphQLUpload;
-module.exports = Upload;
-module.exports = GRAPHQL_MULTIPART_REQUEST_SPEC_URL;
+module.exports = {
+  processRequest,
+  graphqlUploadExpress,
+  GraphQLUpload,
+  graphqlUploadKoa,
+  Upload,
+  GRAPHQL_MULTIPART_REQUEST_SPEC_URL,
+};
